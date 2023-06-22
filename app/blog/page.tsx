7c8de602aa9d7251c6dca6f9ba982d0300c3e8d7
Nikita Peplov <npeplov@yandex.ru>
2023-06-22 14:28:23 +0300

    [+] serch using useState

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { getAllPosts } from "../../services/getPosts";
 import { LoadSpinner } from "@/components/loader/LoadSpinner";
 import { Posts } from "@/components/Posts";
+import { PostSearch } from "@/components/PostSearch";
 
 export const metadata: Metadata = {
   title: "Blog | Next App",
@@ -25,6 +26,7 @@ export default function Blog() {
   return (
     <>
       <h1>Blog page</h1>
+      <PostSearch onSearch={setPosts}/>
       {loading ? <LoadSpinner /> : <Posts posts={posts} />}
     </>
   );
--- services/getPosts.ts
+++ services/getPosts.ts
@@ -7,7 +7,7 @@ export const getAllPosts = async () => {
 }
 
 export const getPostsBySearch = async (search: string) => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts${search}`);
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts?q=${search}`);
 
   if (!response.ok) throw new Error("Fetch posts failed");
 
